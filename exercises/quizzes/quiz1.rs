58b586d2fd9962769965a439f3f04ea8f461a30b
theharshpat <harsh.patel.magic105@gmail.com>
2026-05-22 21:17:09 +0530

    quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(no_of_apples: u32) -> u32 {
+    if no_of_apples > 40 {
+        no_of_apples
+    } else {
+        no_of_apples * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
